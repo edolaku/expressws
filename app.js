cbf40d349d6da39cdab17c3fd239d22f2b1c2979
edolaku <edoooq@gmail.com>
2021-11-28 20:27:21 +0700

    express js materi

---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app
+    .get('/', (req, res) => {
+        res.send('Hello World!')
+    })
+    .get('/about', (req, res) => {
+        res.send('Hello World! About')
+    })
+
+    .listen(port, () => {
+        console.log(`Example app listening at http://localhost:${port}`)
+    })
+
+
+// // Membuat secara lebih ringkas dibanding appcopy.js
+// const http = require("http");
+// const fs = require("fs");
+// const port = 3000;
+
+// // buat fungsi render untuk membuka halaman
+// const renderHTML = (path, res) => {
+//     fs.readFile(path, (err, data) => {
+//         if (err) {
+//             res.writeHead(404);
+//             res.write("Error: File not found");
+//         } else {
+//             res.write(data);
+//         }
+//         res.end();
+//     });
+// };
+
+// http
+//     .createServer((req, res) => {
+//         const url = req.url;
+
+//         res.writeHead(200, {
+//             "Content-Type": "text/html",
+//         });
+//         if (url === "/about") {
+//             renderHTML("./about.html", res)
+//         } else if (url === "/contact") {
+//             renderHTML("./contact.html", res);
+//         } else {
+//             renderHTML("./index.html", res);
+//         }
+//     })
+
+//     .listen(port, () => {
+//         console.log(`Server is listening on port ${port}`);
+//     })
